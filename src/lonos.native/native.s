29fd2b1ba651f9e3dd37e12ba651a9a4feb95eb3
Sebastian Loncar <Arakis@users.noreply.github.com>
2018-11-19 02:43:39 +0100

    Update native.s

--- src/lonos.native/native.s
+++ src/lonos.native/native.s
@@ -3,14 +3,12 @@ mov bx, 0x3c3c         ; attrib/char of smiley
 mov eax, 0x0b8000      ; note 32 bit offset
 mov word [ds:eax], bx
 ret
-;jmp $
 
 test-proc2:
 mov bx, 0x3d3d         ; attrib/char of smiley
 mov eax, 0x0b8002      ; note 32 bit offset
 mov word [ds:eax], bx
 ret
-;jmp $
 
 bochs_debug:
 xchg bx, bx
