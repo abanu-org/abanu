f677a065b0a11e3a689936276b141d653a467c2d
Sebastian Loncar <Arakis@users.noreply.github.com>
2018-11-19 02:43:19 +0100

    Update native.s

--- src/lonos.native/native.s
+++ src/lonos.native/native.s
@@ -1,11 +1,11 @@
-proc1:
+test-proc1:
 mov bx, 0x3c3c         ; attrib/char of smiley
 mov eax, 0x0b8000      ; note 32 bit offset
 mov word [ds:eax], bx
 ret
 ;jmp $
 
-proc2:
+test-proc2:
 mov bx, 0x3d3d         ; attrib/char of smiley
 mov eax, 0x0b8002      ; note 32 bit offset
 mov word [ds:eax], bx
