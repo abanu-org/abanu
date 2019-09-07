1d2201c6000470c450e55b2a36dbeb2f5d1c7a02
Sebastian Loncar <sebastian.loncar@gmail.com>
2019-09-07 02:14:43 +0200

    arguments

--- src/lonos.kernel.core/Scheduling/Thread.cs
+++ src/lonos.kernel.core/Scheduling/Thread.cs
@@ -1,5 +1,4 @@
-﻿using lonos.Kernel.Core.Elf;
-using lonos.Kernel.Core.Interrupts;
+﻿using lonos.Kernel.Core.Interrupts;
 using lonos.Kernel.Core.MemoryManagement;
 using lonos.Kernel.Core.PageManagement;
 using lonos.Kernel.Core.Processes;
@@ -56,47 +55,6 @@ namespace lonos.Kernel.Core.Scheduling
 
     public class UserThread : Thread
     {
-    }
-
-    public class Service
-    {
-
-        public Process Process;
-
-        public Service(Process proc)
-        {
-            this.Process = proc;
-        }
-
-        // Methods is always called within Interrupt with Interrupt disabled
-        public unsafe void SwitchToThreadMethod(uint arg0)
-        {
-            var elf = KernelElf.FromSectionName(Process.Path);
-            var methodAddr = GetEntryPointFromElf(elf);
-            var cThread = Scheduler.GetCurrentThread();
-            var th = Scheduler.CreateThread(Process, new ThreadStartOptions(methodAddr) { ArgumentBufferSize = 4 });
-            th.SetArgument(0, arg0);
-
-            // Connect Threads
-            cThread.ChildThread = th;
-            cThread.Status = ThreadStatus.Waiting;
-            th.ParentThread = cThread;
-
-            //th.StackState->Stack.ECX = arg0;
-
-            th.Start();
-            Scheduler.SwitchToThread(th.ThreadID);
-        }
-
-        private unsafe static Addr GetEntryPointFromElf(ElfHelper elf)
-        {
-            var symName = "System.Void lonos.Kernel.Program::Func1(System.UInt32)"; // TODO
-            var sym = elf.GetSymbol(symName);
-            if (sym == (ElfSymbol*)0)
-                return Addr.Zero;
-            return sym->Value;
-        }
-
     }
 
 }
---
+++ src/lonos.kernel.core/Syscalls/Service.cs
@@ -0,0 +1,56 @@
+﻿using lonos.Kernel.Core.Elf;
+using lonos.Kernel.Core.Processes;
+
+namespace lonos.Kernel.Core.Scheduling
+{
+    public class Service
+    {
+
+        public Process Process;
+
+        public Service(Process proc)
+        {
+            this.Process = proc;
+        }
+
+        // Methods is always called within Interrupt with Interrupt disabled
+        public unsafe void SwitchToThreadMethod(uint arg0)
+        {
+            var elf = KernelElf.FromSectionName(Process.Path);
+            var methodAddr = GetEntryPointFromElf(elf);
+            var th = CreateThread(methodAddr, 4);
+            SwitchToThread(th);
+        }
+
+        public Thread CreateThread(uint methodAddr, uint argumentBufferSize)
+        {
+            return Scheduler.CreateThread(Process, new ThreadStartOptions(methodAddr) { ArgumentBufferSize = argumentBufferSize });
+        }
+
+        public unsafe void SwitchToThread(Thread th)
+        {
+            var cThread = Scheduler.GetCurrentThread();
+
+            // Connect Threads
+            cThread.ChildThread = th;
+            cThread.Status = ThreadStatus.Waiting;
+            th.ParentThread = cThread;
+
+            //th.StackState->Stack.ECX = arg0;
+
+            th.Start();
+            Scheduler.SwitchToThread(th.ThreadID);
+        }
+
+        private unsafe static Addr GetEntryPointFromElf(ElfHelper elf)
+        {
+            var symName = "System.Void lonos.Kernel.Program::Func1(System.UInt32)"; // TODO
+            var sym = elf.GetSymbol(symName);
+            if (sym == (ElfSymbol*)0)
+                return Addr.Zero;
+            return sym->Value;
+        }
+
+    }
+
+}
