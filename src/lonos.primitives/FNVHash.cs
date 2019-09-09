cd4028d438d263012279c6542cbd50f31bdab23f
Sebastian Loncar <sebastian.loncar@gmail.com>
2019-09-10 00:28:37 +0200

    added FNV Hash

---
+++ src/lonos.primitives/FNVHash.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lonos
+{
+
+    /// <summary>
+    /// Fast Hash Algorithm
+    /// </summary>
+    public static class FNVHash
+    {
+        public static int FNV32(params byte[] data)
+        {
+            unchecked
+            {
+                const int p = 16777619;
+                int hash = (int)2166136261;
+
+                for (int i = 0; i < data.Length; i++)
+                    hash = (hash ^ data[i]) * p;
+
+                hash += hash << 13;
+                hash ^= hash >> 7;
+                hash += hash << 3;
+                hash ^= hash >> 17;
+                hash += hash << 5;
+                return hash;
+            }
+        }
+
+        public unsafe static int ComputeInt32(byte* data, int length)
+        {
+            unchecked
+            {
+                const int p = 16777619;
+                int hash = (int)2166136261;
+
+                for (int i = 0; i < length; i++)
+                    hash = (hash ^ data[i]) * p;
+
+                hash += hash << 13;
+                hash ^= hash >> 7;
+                hash += hash << 3;
+                hash ^= hash >> 17;
+                hash += hash << 5;
+                return hash;
+            }
+        }
+    }
+
+}
