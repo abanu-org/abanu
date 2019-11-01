c2eb969a7dd6a5b3c9a5fb2f774edec3c3ddf94f
Sebastian Loncar <sebastian.loncar@gmail.com>
2019-11-01 11:27:20 +0100

    appveyor build

--- src/Abanu.Tools.Build/Program.cs
+++ src/Abanu.Tools.Build/Program.cs
@@ -211,6 +211,7 @@ namespace Abanu.Tools.Build
             {
                 start.RedirectStandardOutput = true;
                 start.RedirectStandardError = true;
+                start.RedirectStandardInput = true;
                 start.UseShellExecute = false;
             }
 
